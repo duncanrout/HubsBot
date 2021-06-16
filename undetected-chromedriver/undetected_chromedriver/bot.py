3274d56b18dc23ff2266f738b8e1c1eed12ae3a8
Duncan Rout <65206033+duncanrout@users.noreply.github.com>
2021-06-16 07:40:00 -0500

    C:\Users\dunca\OneDrive\Documents\GitHub\HubsBot

--- undetected-chromedriver/undetected_chromedriver/bot.py
+++ undetected-chromedriver/undetected_chromedriver/bot.py
@@ -1,14 +1,15 @@
 import undetected_chromedriver.v2 as uc
-#new imports
-from selenium import webdriver
+#from selenium import webdriver
+options = uc.ChromeOptions()
 
-## setting profile
-#options.user_data_dir = "c:\\temp\\profile"
-options.user_data_dir = "C:\\Users\\Duncan.Rout\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\Default"
+def signIn():
+    ##profile work
+    #options.user_data_dir = "C:\\Users\\Duncan.Rout\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\Default"
 
-## another way to set profile is the below (which takes precedence if both variants are used
-#options.add_argument('--user-data-dir=c:\\temp\\profile2')
-print(5)
+    #profile home
+    options.user_data_dir = "C:\\Users\\dunca\\AppData\\Local\\Google\\Chrome\\User Data\\Default"
+    
+signIn()
 
 # just some options passing in to skip annoying popups
 options.add_argument('--no-first-run --no-service-autorun --password-store=basic')
@@ -17,9 +18,4 @@ driver = uc.Chrome(options=options)
 with driver:
     #driver.get('https://app.hubspot.com/login/?hsCtaTracking=d82c5d82-821c-4c0d-86f5-6c34dced0e6d%7C16494015-d90f-47fc-adc3-593e2e1bdfa0')
     driver.get('https://app.hubspot.com/contacts/1734343/objects/0-2/views/5765433/list')
-
-#email = driver.find_element_by_id("username")
-#password = driver.find_element_by_id("password")
-
-#driver.find_element_by_id("loginBtn").click()
-domainLink = driver.find_element_by_xpath("/html/body/div[1]/div[1]/div[2]/form/button[1]".click()
+    driver.find_element_by_xpath("//input[@placeholder ='Full Name' and @type = 'text']")
